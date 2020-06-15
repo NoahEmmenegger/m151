e2e32a07f9cc6e9b92a11466a122c86246a13ca2
Noah Emmenegger <noahemmenegger.business@gmail.com>
2020-06-15 12:59:47 +0200

    data

--- m151.data/DemoContext.cs
+++ m151.data/DemoContext.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using m151.data.Model;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace m151.data
 {
     public class DemoContext : DbContext
     {
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        public DemoContext(DbContextOptions<DemoContext> options)
+            :base(options)
+        { }
     }
 }
--- m151/TestContext.cs
+++ m151/TestContext.cs
@@ -1,19 +1,19 @@
-﻿using ch.gibz.m151.demo.api.Model;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using ch.gibz.m151.demo.api.Model;
+//using Microsoft.EntityFrameworkCore;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 
-namespace m151
-{
-    public class TestContext
-    {
-        public TestContext(DbContextOptions<TestContext> options)
-          : base(options)
-        {
-        }
+//namespace m151
+//{
+//    public class TestContext
+//    {
+//        public TestContext(DbContextOptions<TestContext> options)
+//          : base(options)
+//        {
+//        }
 
-        public DbSet<Test> TodoItem { get; set; }
-    }
-}
+//        public DbSet<Test> TodoItem { get; set; }
+//    }
+//}
